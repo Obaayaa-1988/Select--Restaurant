25fbb7b2df162bb4a208691f696a96aae71d4efe
Obaayaa-1988 <aquaicoe31@gmail.com>
2021-12-14 18:15:53 +0000

    media queries

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 const hamburger = document.getElementById('hamburger');
-const navLink = document.getElementById('nav-ul');
+const navUl = document.getElementById('nav-ul');
 
-hamburger.accessKeyLabel('click', () => {
-    navLink.classList.toggle('show');
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
+
+/*const modal = document.getElementById('modal');
+const button = document.getElementById('btns');
+const close = document.getElementById('close');
+
+button.addEventListener('click', () => {
+    modal.style.display = 'block';
 })
+
+close.addEventListener('click', () => {
+    modal.style.display = 'none';
+})*/
+
+/*window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}*/
+
+/*let toggled = false;
+function toggleButton(){
+    if(!toggled){
+        toggled = true;
+        document.getElementById('modal').style.display = 'none'
+        return;
+    }
+    if(toggled) {
+        toggled = false;
+        document.getElementById('modal').style.display = 'block'
+        return;
+    }
+}*/
+
